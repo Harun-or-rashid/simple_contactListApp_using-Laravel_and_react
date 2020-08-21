5f6935aac6cf27523389d30b0dc48dc4c7374c9b
JoHarun-or-rashid <harun35-2093@diu.edu.bd>
2020-08-21 22:26:53 +0600

    Contact change to Contacts.js

--- resources/js/components/Contact.js
+++ resources/js/components/Contact.js
@@ -1,48 +1,23 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import loader from 'sass-loader';
-import { bind } from 'lodash';
-import Axios from 'axios';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-class Contact extends Component {
-constructor (props){
-super(props)
-
-this.state={
-    contacts:[],
-    loader: true
-}
-this.fetchContacts=this.fetchContacts.bind(this);
-this.componentDidMount=this.componentDidMount.bind(this);
-}
-    fetchContacts(event){
-        const res= axios.get('/contact')
-        .then(function(res){
-            if(res.data.sttatus==200){
-                this.setState({contacts:res.data.contacts});
-                this.setState({loader:true});
-            }
-            console.log(res);
-        }) .catch(function (res) {
-            console.log(res);
-            });
+export default class Contact extends Component {
+    static propTypes = {
+        prop: PropTypes
     }
- componentDidMount(){
-     this.fetchContacts();
- }
-   
+
     render() {
-        if(!this.state.loader==this.state.contacts){
-            return <h3>Loading...</h3>
-        }
+        const {contact}=this.props;
         return (
-            <div>
-                <h1>hi</h1>
+            <div className="card">
+                <div className="card-body">
+                    <div className="row">
+<                       div className="col-md-2 ">
+                            sp
+                        </div>
+                    </div>
+                </div>
             </div>
-        );
+        )
     }
 }
-
-Contact.propTypes = {};
-
-export default Contact;
---
+++ resources/js/components/Contacts.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import loader from 'sass-loader';
+import { bind, concat } from 'lodash';
+import Axios from 'axios';
+
+class Contact extends Component {
+constructor (props){
+super(props)
+
+this.state={
+    contacts:[],
+    loading: true
+}
+this.fetchContacts=this.fetchContacts.bind(this);
+this.componentDidMount=this.componentDidMount.bind(this);
+}
+    fetchContacts(event){
+        const res= axios.get('/contact')
+        .then(function(response){
+            if(response.data.sttatus==200){
+                this.setState({loading:false});
+                this.setState({contacts:res.data.contacts});
+            }
+            
+            console.log(response);
+        }) .catch(function (response) {
+            console.log(response);
+            });
+    }
+ componentDidMount(){
+     this.fetchContacts();
+ }
+ 
+   
+    render() {
+        // list=this.state.contacts.map((contact)=>
+        // <li>{concat}</li>
+        //  );
+        if(this.state.loading){
+            return <h3>Loading...</h3>
+        }
+        return (
+            <div>
+                <h1>hi</h1>
+        {this.state.contact.map(contact=>(
+            <Contact contact={contact} key={contact.id}/>
+        ))}
+            </div>
+        );
+    }
+}
+
+Contact.propTypes = {};
+
+export default Contact;
